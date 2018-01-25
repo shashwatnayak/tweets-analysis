c18789908eef38fe365400a1d4bdc120d34625da
Shashwat Nayak <shashwatnayak@outlook.com>
2018-01-25 20:18:27 +0530

    created simple version

--- twitstream.py
+++ twitstream.py
@@ -1,23 +1,25 @@
 
+#Created simple one in teststream.py
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler, Stream
 
 #Credentials
-ACCESS_TOKEN = "Your value"
-ACCESS_TOKEN_SECRET = "Your value"
-CONSUMER_KEY = "Your value"
-CONSUMER_SECRET = "Your value"
+ACCESS_TOKEN = '878103708357148673-T3qTnMtKUEkCPN7zKUPjmK0zE4TCMSk'
+ACCESS_TOKEN_SECRET = 'M5IOVTDQvKcaE7NQLSL94Scrol2OtmPpnSkQT7aav6xT1'
+CONSUMER_KEY = 'z2jh1jxHEWHtIc0I0fZUmWalo'
+CONSUMER_SECRET = 'TCTLwdn7282qngW9Pq655fWhJ7KYtpzWSVXJqy85MUsxlfskQa'
 
 #Listener object
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
         """Just prints out incoming data to the stdout"""
-        print data
+        print (data)
         return True
 
     def on_error(self, status):
-        print status
+        print (status)
 
 if __name__ == '__main__':
 
