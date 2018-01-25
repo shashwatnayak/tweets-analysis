869a931c242d57c475b311c047e168b49a4e6400
Shashwat Nayak <shashwatnayak@outlook.com>
2018-01-25 18:27:52 +0530

    code for extracting tweets

---
+++ twitanalysis.py
@@ -0,0 +1,16 @@
+#Imports
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#We load json data and store all tweets in a list.
+tweets_data_path = './data/twitter_API_data.txt'
+
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+for line in tweets_file:
+    try:
+        tweet = json.loads(line)
+        tweets_data.append(tweet)
+    except:
+        continue
